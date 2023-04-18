2e07ca09089cb26e865ced3464ac63e5acf4290b
Nate Foster <nate.s.foster@gmail.com>
2023-04-18 08:51:39 -1000

    export module and write tests

---
+++ problems/mergeAlternately.js
@@ -0,0 +1,25 @@
+/* 1768. Merge Strings Alternately
+ *
+ * You are given two strings word1 and word2.
+ * Merge the strings by adding letters in alternating order,
+ * starting with word1.
+ * If a string is longer than the other, append the additional letters
+ * onto the end of the merged string.
+ *
+ * Return the merged string
+ *
+ * Example 1:
+ * Input: word1 = "abc", word2 = "pqr"
+ * Output: "apbqcr"
+ *
+ * Example 2:
+ * Input: word1: "ab", word2 = "pqrs"
+ * Output: "aqbqrs"
+ *
+ */
+
+const mergeAlternately = (word1, word2) => {
+
+};
+
+module.exports = mergeAlternately;
