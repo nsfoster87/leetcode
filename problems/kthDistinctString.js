ddde39d1440d6187d0b493b09b144fca5b95327c
Nate Foster <nate.s.foster@gmail.com>
2023-04-27 10:46:51 -1000

    export module and write tests

---
+++ problems/kthDistinctString.js
@@ -0,0 +1,37 @@
+/* 2053. Kth Distinct String in an Array
+ *
+ * A distinct string is a string that is present only once in an array.
+ *
+ * Given an array of strings arr, and an integer k, return the kth
+ * distinct string present in arr. If there are fewer than k distinct strings,
+ * return an empty string "".
+ *
+ * Note that the strings are considered in the order in which they appear in the array.
+ *
+ * Example 1:
+ * Input: arr = ["d", "b", "c", "b", "c", "a"], k = 2
+ * Output: "a"
+ * Explanation: The only distinct strings in arr are "d" and "a",
+ * "d" appears 1st, so it is the 1st distinct string. "a" appears 2nd, so
+ * it is the 2nd distinct string.
+ *
+ * Example 2:
+ * Input: arr = ["aaa", "aa", "a"], k = 1
+ * Ouput: "aaa"
+ *
+ * Example 3:
+ * Input: arr = ["a", "b", "a"], k = 3
+ * Output: ""
+ *
+ * Constraints:
+ * 1 <= k <= arr.length <= 1000
+ * 1 <= arr[i].length <= 5
+ * arr[i] consists of lowercase English letters
+ *
+ */
+
+const kthDistinct = (arr, k) => {
+
+};
+
+module.exports = kthDistinct;
