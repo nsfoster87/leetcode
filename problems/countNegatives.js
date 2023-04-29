e6aa8bc27d1881d544532335f621fead1f015b6e
Nate Foster <nate.s.foster@gmail.com>
2023-04-29 11:42:04 -1000

    export module and write tests

---
+++ problems/countNegatives.js
@@ -0,0 +1,19 @@
+// 1351. Count Negative Numbers in a Sorted Matrix
+
+// Given an m x n matrix grid which is sorted in non-increasing order both
+// row-wise and column-wise, return the number of negative numbers in the grid
+
+// Example 1:
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+
+// Example 2:
+// Input: grid = [[3,2],[1,0]]
+// Output: 0
+
+const countNegatives = (grid) => {
+  return 0;
+};
+
+module.exports = countNegatives;
---
+++ testing/countNegatives.test.js
@@ -0,0 +1,11 @@
+const countNegatives = require('../problems/countNegatives');
+
+describe('countNegatives', () => {
+  it('should return the number of negative numbers in a 2d sorted matrix', () => {
+    let grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]];
+    expect(countNegatives(grid)).toBe(8);
+
+    grid = [[3,2],[1,0]];
+    expect(countNegatives(grid)).toBe(0);
+  });
+});
