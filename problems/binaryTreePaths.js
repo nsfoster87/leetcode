056ad26e2824ce7fc8da6d8a5f55b36082a914db
Nate Foster <nate.s.foster@gmail.com>
2023-05-19 13:19:57 -0400

    refactor

--- problems/binaryTreePaths.js
+++ problems/binaryTreePaths.js
@@ -29,37 +29,14 @@ class TreeNode {
 }
 
 const binaryTreePaths = (root) => {
-  // do a depth first search,
-  // finding all the leafs, while recording the path
-
-  // for tree [1,2,3,4,5,null,null,null,null,10]:
-  // from bottom up:
-  // 10: returns ["10"]
-  // 5 and 4: returns ["5->10"], ["4"]
-  // 2 and 3: returns ["2->5->10", "2->4"], ["3"]
-  // 1: returns ["1->2->5->10", "1->2->4", "1->3"]
-
-  if (!root.left && !root.right) {
-    return [`${root.val}`];
-  } else if (root.left && root.right) {
-    const paths = binaryTreePaths(root.left)
-      .map(path => `${root.val}->${path}`)
-      .concat(binaryTreePaths(root.right)
-        .map(path => `${root.val}->${path}`));
-    console.log(paths);
-    return paths;
-  } else {
-    const child = root.left ? root.left : root.right;
-    return binaryTreePaths(child).map(path => `${root.val}->${path}`);
+  if (!root.left && !root.right) return [`${root.val}`];
+  if (root.left && root.right) {
+    const leftPath = binaryTreePaths(root.left).map(path => `${root.val}->${path}`)
+    const rightPath = binaryTreePaths(root.right).map(path => `${root.val}->${path}`);
+    return leftPath.concat(rightPath);
   }
-  // else if root has both children,
-    // leftPath = binaryTreePaths(leftChild).map(path => "rootString -> path");
-    // rightPath = binaryTreePaths(rightChild).map(path => "rootString -> path");
-    // return leftPath.concat(rightPath);
-  // else if root has left child:
-    // return binaryTreePaths(leftChild).map(path => "rootString -> path");
-  // else
-    // return binaryTreePaths(rightChild).map(path => "rootString -> path");
+  const child = root.left ? root.left : root.right;
+  return binaryTreePaths(child).map(path => `${root.val}->${path}`);
 };
 
 module.exports = { binaryTreePaths, TreeNode };
