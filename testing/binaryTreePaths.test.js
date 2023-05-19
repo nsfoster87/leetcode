d60f9e39383dd9a499f709b00cde7cf73aa845aa
Nate Foster <nate.s.foster@gmail.com>
2023-05-19 13:02:05 -0400

    adjust tests to use TreeNodes

--- testing/binaryTreePaths.test.js
+++ testing/binaryTreePaths.test.js
@@ -2,15 +2,25 @@ const { TreeNode, binaryTreePaths } = require('../problems/binaryTreePaths');
 
 describe('binaryTreePaths', () => {
   it('should return all root-to-leaf paths in any order', () => {
-    let ouput = binaryTreePaths([1,2,3,null,5]);
+    let five = TreeNode(5, null, null);
+    let three = TreeNode(3, null, null);
+    let two = TreeNode(2, null, five);
+    let root = TreeNode(1, two, three);
+
+    let ouput = binaryTreePaths(root);
     let expected = ["1->2->5", "1->3"];
     expect(expected.every(path => output.contains(path))).toBe(true);
+
+    let four = TreeNode(4, null, null);
+    two.left = four;
+    let ten = TreeNode(10, null, null);
+    five.left = ten;
 
     output = binaryTreePaths([1,2,3,4,5,null,null,null,null,10]);
     expected = ["1->2->4", "1->2->5->10", "1->3"];
     expect(expected.every(path => output.contains(path))).toBe(true);
   });
   it('should return path for tree with one node', () => {
-    expect(binaryTreePaths([1])).toEqual(["1"]);
+    expect(binaryTreePaths(TreeNode(1, null, null))).toEqual(["1"]);
   });
 });
