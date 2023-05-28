7d4a1e964d4a7fc950eb08bde77adfa046adba55
Nate Foster <nate.s.foster@gmail.com>
2023-05-28 12:48:23 -0400

    more notes for geometric Sequence approach

--- problems/new21Game.js
+++ problems/new21Game.js
@@ -29,6 +29,40 @@ const new21Game = (n, k, maxPts) => {
   // 1 + 1 + 1 + 1 + 2 = 6  ->  result += 0.1**5
   // 1 + 4                  ->  result += 0.1**2
 
+  // Simple version (need to go back and retool for 20-> 21 in subsequents, etc):
+  // while max - k <= 0, depth += 1, max += max,
+  // iters = max - k,
+  // multiple = max > n ? n + 1 - k : max - k
+  // example:
+  //
+  // n = 21, k = 17, maxPts = 10
+  // 1 card (depth 1):
+  // let newMax = maxPts (10)
+  // while (newMax - (k-1) <= 0) {
+      // depth += 1; newMax += max (depth: 2, newMax = 20)
+  // }
+
+  // 2 cards (depth 2):
+  // n = 21, (k-1) = 16, newMax = 20, (n - (k-1)) = 5
+  // multiple = 4 ...(newMax < n, therefore multiple = newMax - (k-1))
+  // result += (0.1)**depth * geoSequence (1)
+
+  // continue while geoSequence is > 0
+  // depth += 1, newMax += max
+  // depth = 3, newMax = 30, multiple = 5 (newMax > n, multiple = n - (k-1))
+  // geoSequence = 1 + 1 + 1 + 1 + 1 + 1 (1 * (k-1) - max)
+  //
+  // depth = 4:
+  // geoSequence = 0 + 1 + (1 + 1) + (1 + (1 + 1)) + (1 + (1 + (1 + 1))) + (1 + (1 + (1 + (1 + 1))))
+  // OR = 0 + 1 + 2 + 3 + 4 + 5
+  // depth = 5
+  // geoSequence = 0 + 0 + 1 + (1 + 2) + (1 + 2 + 3) + (1 + 2 + 3 + 4)
+  // depth = 6
+  // geoSequence = 0 + 0 + 0 + 1 + (1 + (1 + 2)) + (1 + (1 + 2) + (1 + 2 + 3))
+  // depth = 7
+  // geoSequence = 0 + 0 + 0 + 0 + 1 + (1 + (1 + (1 + 2)))
+  // this is all just for 1card=10
+
   if (k === 0) return k <= n;
 
   const equalProb = 1 / maxPts;
