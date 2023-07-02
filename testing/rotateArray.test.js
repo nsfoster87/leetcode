429141d34df0dd1a0397771aa0a26ed10b1b4c2c
Nate Foster <nate.s.foster@gmail.com>
2023-07-02 11:59:40 -0400

    rotateArray: export module and write tests

---
+++ testing/rotateArray.test.js
@@ -0,0 +1,15 @@
+const rotate = require('../problems/rotateArray.js');
+
+describe('rotateArray', () => {
+  it('should rotate the array in-place k times to the right', () => {
+    let nums = [1,2,3,4,5,6,7];
+    let k = 3;
+    rotate(nums, k)
+    expect(nums).toEqual([5,6,7,1,2,3,4]);
+
+    nums = [-1,-100,3,99];
+    k = 2;
+    rotate(nums, k);
+    expect(nums).toEqual([3,99,-1,-100]);
+  })
+});
