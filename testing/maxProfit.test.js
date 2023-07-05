09df289495e8e96a2e724d0b5dcfb173960c4f22
Nate Foster <nate.s.foster@gmail.com>
2023-07-05 13:57:07 -0400

    maxProfitActive: export module and write tests

--- testing/maxProfit.test.js
+++ testing/maxProfit.test.js
@@ -1,4 +1,5 @@
 const maxProfit = require('../problems/maxProfit');
+const maxProfitActive = require('../problems/maxProfitActive');
 
 describe('maxProfit', () => {
   it('should return maximum profit given stock prices for each day', () => {
@@ -12,5 +13,16 @@ describe('maxProfit', () => {
   it('should return max profit if stock price fluctuates up and down', () => {
     const prices = [7,4,2,5,3,8,1];
     expect(maxProfit(prices)).toBe(6);
-  })
+  });
+});
+
+describe('maxProfitActive', () => {
+  it('should return the maximum profit from buying and selling a stock on any days', () => {
+    expect(maxProfitActive([7,1,5,3,6,4])).toBe(7);
+    expect(maxProfitActive([1,3,5,4,10])).toBe(10);
+    expect(maxProfitActive([1,2,3,4,5])).toBe(4);
+  });
+  it('should return zero profit if there is no profit potential', () => {
+    expect(maxProfitActive([7,6,4,3,1])).toBe(0);
+  });
 });
