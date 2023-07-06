90b6c574919b553d27756644577dbdd4909e1c30
Nate Foster <nate.s.foster@gmail.com>
2023-07-06 13:39:30 -0400

    implement O(n) iterative solution

--- problems/canJump.js
+++ problems/canJump.js
@@ -6,16 +6,14 @@
 // Return true if you can reach the last index, or false otherwise.
 
 const canJump = (nums) => {
-  // zeroes are the main concern, so
-  // iterate through til you find a zero,
-  // keeping track of the biggest leap you can take...
-  // if the biggest leap gets to zero and you're at a zero, game over
-  if (nums[0] >= nums.length - 1) return true;
-  for (let i = nums[0]; i > 0; i--) {
-    if (canJump(nums.slice(i))) return true;
+  let biggestJump = nums[0];
+  for (let i = 0; i < nums.length-1; i++) {
+    biggestJump = Math.max(nums[i], biggestJump);
+    if (biggestJump < 1) return false;
+    biggestJump--;
   }
 
-  return false;
+  return true;
 };
 
 module.exports = canJump;
