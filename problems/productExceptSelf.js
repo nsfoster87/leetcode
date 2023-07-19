93c082f575b8d73c8b1b29d3205ae332ec571af5
Nate Foster <nate.s.foster@gmail.com>
2023-07-18 21:59:25 -0400

    implement productExceptSelf

--- problems/productExceptSelf.js
+++ problems/productExceptSelf.js
@@ -9,7 +9,18 @@
 // and without using the division operation.
 
 const productExceptSelf = (nums) => {
-
+  let result = [];
+  let accumulator = 1;
+  for (let i = 0; i < nums.length; i++) {
+      result[i] = accumulator;
+      accumulator *= nums[i];
+  }
+  accumulator = 1;
+  for (let j = nums.length - 1; j >= 0; j--) {
+      result[j] *= accumulator;
+      accumulator *= nums[j];
+  }
+  return result;
 };
 
 module.exports = productExceptSelf;
