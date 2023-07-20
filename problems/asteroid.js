6fc6bc369b9159aa4fdad191335906c508101927
Nate Foster <nate@Nates-MacBook-Air.local>
2023-07-20 10:43:07 -0400

    export module and write tests

---
+++ problems/asteroid.js
@@ -0,0 +1,18 @@
+// 725. Asteroid Collision
+
+// We are given an array asteroids of integers representing asteroids in a row.
+
+// For each asteroid, the absolute value represents its size, a
+// nd the sign represents its direction (positive meaning right, negative meaning left). 
+// Each asteroid moves at the same speed.
+
+// Find out the state of the asteroids after all collisions. 
+// If two asteroids meet, the smaller one will explode. 
+// If both are the same size, both will explode. 
+// Two asteroids moving in the same direction will never meet.
+
+const asteroidCollision = (asteroids) => {
+
+};
+
+module.exports = asteroidCollision;
