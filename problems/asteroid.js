a65019630426f4c693c06111b0ac1e40a87ade55
Nate Foster <nate@Nates-MacBook-Air.local>
2023-07-20 10:43:42 -0400

    pseudocode

--- problems/asteroid.js
+++ problems/asteroid.js
@@ -12,7 +12,19 @@
 // Two asteroids moving in the same direction will never meet.
 
 const asteroidCollision = (asteroids) => {
-
+    // create a result array
+    // iterate through the asteroids array
+    // for each asteroid
+      // if asteroid is positive,
+        // add it to the result array
+      // if asteroid is negative
+        // if results array is empty or last elem is negative,
+        // add asteroid to result array
+        // else
+        // while abs(asteroid) is bigger than or equal to the last element in the results array and last elem is > 0,
+          // pop off last result
+          // if they're equal, break;
+    // return result
 };
 
 module.exports = asteroidCollision;
