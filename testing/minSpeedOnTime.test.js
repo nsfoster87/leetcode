f4383492ece4142c7066d8ae22949bb7828a4cd2
Nate Foster <nate@Nates-MacBook-Air.local>
2023-07-26 11:16:58 -0400

    export module and write tests

---
+++ testing/minSpeedOnTime.test.js
@@ -0,0 +1,16 @@
+const minSpeedOnTime = require('../problems/minSpeedOnTime');
+
+describe('minSpeedOnTime', () => {
+  it('should return the minimum speed for an hour greater than n', () => {
+    expect(minSpeedOnTime([1,3,2], 6)).toBe(1);
+  });
+  it('should return the minimum speed for a floating point hour', () => {
+    expect(minSpeedOnTime([1,3,2], 2.7)).toBe(3);
+  });
+  it('should return -1 if it is not possible to get there on time', () => {
+    expect(minSpeedOnTime([1,3,2], 1.9)).toBe(-1);
+  });
+  it('should return the minimum speed for an hour equal to n', () => {
+    expect(minSpeedOnTime([1,3,2], 3)).toBe(3);
+  });
+});
