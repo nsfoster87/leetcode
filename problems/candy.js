e86ce72f798ecdfea17e2b2fced160a624355e7b
Nate Foster <nate@Nates-MacBook-Air.local>
2023-07-27 17:24:06 -0400

    implement candy

--- problems/candy.js
+++ problems/candy.js
@@ -10,25 +10,26 @@
 // Return the minimum number of candies you need to have to distribute the candies to the children.
 
 const candy = (ratings) => {
-  // [7, 1, 6, 5, 4, 5, 5, 0, 1, 3] (ex ratings)
-  // 
-  // keep an array of the amount of candy doled out to each child.
-  // initialize a prevRating = -1
-  // for each rating in ratings:
-    // if the previous rating is lower than current
-      // if current === 0, set candies[current] = 1
-      // else set candies[current] equal to candies[current-1] + 1
-    // if the previous rating is gte current
-      // set candies[current] = 1
-      // set backPointer = current-1
-      // while (backPointer >= 0 && candies[backPointer] === candies[current])
-        // increment candies[backPointer]
-        // backPointer--
-    // if prevRating is equal to current
-      // determine which side has a longer grouping of larger
-      // numbers, and make that one lesser
-    // set prevRating equal to ratings[current]
-      
+
+  const candies = [];
+  let prevRating = -1;
+
+  for (let i = 0; i < ratings.length; i++) {
+    if (prevRating < ratings[i]) {
+      candies[i] = i !== 0 ? candies[i-1] + 1 : 1;
+    } else {
+      candies[i] = 1
+      backPointer = i - 1;
+      while (backPointer >= 0 && candies[backPointer] === candies[backPointer+1]) {
+        if (ratings[backPointer] === ratings[backPointer+1]) break;
+        candies[backPointer]++;
+        backPointer--;
+      }
+    }
+    prevRating = ratings[i];
+  }
+
+  return candies.reduce((sum, candy) => sum + candy);
 };
 
 module.exports = candy;
