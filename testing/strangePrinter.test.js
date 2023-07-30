07ada453015194b7d3ace79f901a6a9e5249233e
Nate Foster <nate.s.foster@gmail.com>
2023-07-30 09:49:01 -0400

    export module and write tests

---
+++ testing/strangePrinter.test.js
@@ -0,0 +1,12 @@
+const strangePrinter = require('../problems/strangePrinter');
+
+describe('strangePrinter', () => {
+  it('should return the number of turns the printer needs to take to print string s', () => {
+    expect(strangePrinter('aaabbb')).toBe(2);
+    expect(strangePrinter('aba')).toBe(2);
+    expect(strangePrinter('abababa')).toBe(4);
+  });
+  it('should return 1 for an input string of one character', () => {
+    expect(strangePrinter('a')).toBe(1);
+  });
+});
