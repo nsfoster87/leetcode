10ad95a6c33fef71c06939f089ab0a31b474950e
Nate Foster <nate.s.foster@gmail.com>
2023-08-07 11:02:10 -0400

    export module and write tests

---
+++ problems/rainwater.js
@@ -0,0 +1,32 @@
+// 42. Trapping Rain Water
+
+// Given n non-negative integers representing an elevation map
+// where the width of each bar is 1, compute how much water it
+// can trap after raining.
+
+// Example 1:
+//
+// 3-
+// 2-       X
+// 1-   X   XX X
+// 0- X XX XXXXXX
+//
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map is represented by the array 'height'
+//   In this case, 6 units of rainwater are being trapped
+
+// Example 2:
+// Input: height = [4,2,0,3,2,5]
+// Ouput: 9
+
+// Constraints:
+// n == height.length
+// 1 <= n <= 2*10^4
+// 0 <= height[i] <= 10^5
+
+const trap = (height) => {
+
+};
+
+module.exports = trap;
