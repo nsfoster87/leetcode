a5ddeddaaafc12133e12ce6ebadce0ffd3db7f26
Nate Foster <nate.s.foster@gmail.com>
2023-08-07 12:10:42 -0400

    new approach, less memory space

--- problems/rainwater.js
+++ problems/rainwater.js
@@ -26,21 +26,49 @@
 // 0 <= height[i] <= 10^5
 
 const trap = (height) => {
+
   // approach:
   // keep track of total
-  // keep track of an object that has current heights,
-  // as well as that height's gap count
-  // at each new index,
-    // if water needs to be filled in,
-      // fill it in from the bottom up:
-      // take a row minus the next lowest row
-      // and multiply it by the gap count
-      // then remove that row from the object
-      // (up to rows < current index's value)
-      // add this total to the total
-    // set gaps[height[index]] = 0
-    // increment all remaining numbers in gaps
-    // (only gaps heigher than current block height should remain)
+  // keep track of current floor and current wall-height
+  // at each height,
+  // if height is less than current floor,
+    // current floor = height
+  // if height is greater than current floor and wall > 0,
+    // work backwards adding water to total
+    // up to current wall-height
+    // if height is less than current wall-height,
+      // set current floor to height
+    // if it's greater,
+      // set current wall-height to height, as well as floor
+
+   // [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+   // i: 0, floor: 0, wall: 0, total: 0
+   // i: 1, floor: 1, wall: 1, total: 0
+   // i: 2, floor: 0, wall: 1, total: 0
+   // i: 3...
+     // since height[3] === 2 is greater than floor
+     // working backwards until we hit wall...
+     // index of wall === 1
+     // (i-1) - index of wall = 1
+     // 1 * wall = 1
+     // total += ((i-1)-wallIndex)*wallHeight
+   // i: 3, floor: 2, wall: 2, total: 1
+   // i: 4, floor: 1, wall: 2, total: 1
+   // i: 5, floor: 0, wall: 2, total: 1
+   // i: 6...
+     // height[6] === 1, which is greater than floor of 0
+     // height[5] === 0, water += 1 - 0
+     // height[4] === 1, so stop. add water to total
+     // height < wall, so set floor to 1
+   // i: 6, floor: 1, wall: 2, total: 2
+   // i: 7...
+     // height[7] === 2, which is greater than floor of 1,
+     // walk backwards until we hit a wall greater than floor,
+     // or wall
+     // we hit wall at indexOfWall (3)
+     // 7-1 - 3 = 3
+     // multiply 3 * wall - floor (1), and add to total
+     // height > wall, so wall is now height (3) and floor is now (3)
 };
 
 module.exports = trap;
